079445a4e38dc73d1a1dc2e7f9be7b07caea0228
FhRh <yiyop@naver.com>
2024-11-14 16:49:25 +0900

    feat(IngredientMatcher) : 해당 재료가 레시피의 메인 재료인지 확인할 수 있다
    - IngredientDto 수정
    - Recipe와 IngredientDtos를 넘겨 해당 레시피의 메인 재료일시, 명시한다.
    - Dto는 최상위 Dto에 바로 매개변수로 넣어 만들기보다는 차근차근 만들어 나갑니다.
    - 레시피 메인재료 데이터에 접근하는 Repository 생성

--- Server/banchango/src/main/java/com/sundaegukbap/banchango/ingredient/dto/dto/IngredientDto.java
+++ Server/banchango/src/main/java/com/sundaegukbap/banchango/ingredient/dto/dto/IngredientDto.java
@@ -1,19 +1,30 @@
 package com.sundaegukbap.banchango.ingredient.dto.dto;
 
 import com.sundaegukbap.banchango.ingredient.domain.Ingredient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-public record IngredientDto(
-        Long id,
-        String name,
-        String kind,
-        String image
-) {
-    public static IngredientDto of(Ingredient ingredient){
+@AllArgsConstructor
+@Getter
+public class IngredientDto {
+
+    private Long id;
+    private String name;
+    private String kind;
+    private String image;
+    private boolean isMain;
+
+    public static IngredientDto of(Ingredient ingredient) {
         return new IngredientDto(
-                ingredient.getId(),
-                ingredient.getName(),
-                ingredient.getKind(),
-                ingredient.getImage()
+            ingredient.getId(),
+            ingredient.getName(),
+            ingredient.getKind(),
+            ingredient.getImage(),
+            false
         );
+    }
+
+    public void setIsMain(boolean isMain) {
+        this.isMain = isMain;
     }
 }
