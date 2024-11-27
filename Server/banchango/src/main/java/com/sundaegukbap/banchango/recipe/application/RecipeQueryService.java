272b405e0bcc7341958911405bea6023c217ec1f
Due <48638700+Due-IT@users.noreply.github.com>
2024-11-27 11:02:53 +0900

    refactor(RecipeQueryService) : 안쓰는 의존성 제거 (#69)

--- Server/banchango/src/main/java/com/sundaegukbap/banchango/recipe/application/RecipeQueryService.java
+++ Server/banchango/src/main/java/com/sundaegukbap/banchango/recipe/application/RecipeQueryService.java
@@ -13,61 +13,62 @@ import com.sundaegukbap.banchango.recipe.repository.RecommendedRecipeRepository;
 import com.sundaegukbap.banchango.user.domain.User;
 import com.sundaegukbap.banchango.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RecipeQueryService {
+
     private final RecipeRepository recipeRepository;
     private final UserRepository userRepository;
     private final RecommendedRecipeRepository recommendedRecipeRepository;
     private final IngredientMatcher ingredientMatcher;
-    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
     public RecommendedRecipeResponse getRecipeDetail(Long userId, Long recipeId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NoSuchElementException("no user"));
+            .orElseThrow(() -> new NoSuchElementException("no user"));
         Recipe recipe = recipeRepository.findById(recipeId)
-                .orElseThrow(() -> new NoSuchElementException("no recipe"));
+            .orElseThrow(() -> new NoSuchElementException("no recipe"));
 
         return resolveRecipeWithUser(user, recipe);
     }
 
     @Transactional
-    public RecommendedRecipeResponses getRecommendedRecipes(int pageIndex, int pageSize, Long userId) {
+    public RecommendedRecipeResponses getRecommendedRecipes(int pageIndex, int pageSize,
+        Long userId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NoSuchElementException("no user"));
+            .orElseThrow(() -> new NoSuchElementException("no user"));
 
         int firstDataIndex = pageIndex * pageSize;
         List<Recipe> recipes;
-        if(firstDataIndex >= 50){
+        if (firstDataIndex >= 50) {
             recipes = recipeRepository.findRecipesByRandom(pageSize);
         } else {
             PageRequest pageRequest = PageRequest.of(pageIndex, pageSize);
-            List<UserRecommendedRecipe> userRecommendedRecipeList = recommendedRecipeRepository.findAllByUser(pageRequest, user).getContent();
+            List<UserRecommendedRecipe> userRecommendedRecipeList = recommendedRecipeRepository.findAllByUser(
+                pageRequest, user).getContent();
             recipes = userRecommendedRecipeList.stream()
-                    .map(r -> r.getRecipe())
-                    .collect(Collectors.toList());
+                .map(r -> r.getRecipe())
+                .collect(Collectors.toList());
         }
 
         List<RecommendedRecipeResponse> recommendedRecipeResponseList = recipes.stream()
-                .map(recipe -> resolveRecipeWithUser(user, recipe))
-                .collect(Collectors.toList());
+            .map(recipe -> resolveRecipeWithUser(user, recipe))
+            .collect(Collectors.toList());
 
         return RecommendedRecipeResponses.of(recommendedRecipeResponseList);
     }
 
     public RecommendedRecipeResponse resolveRecipeWithUser(User user, Recipe recipe) {
-        HashMap<String, List> ingredientRelation = ingredientMatcher.checkIngredientRelation(user, recipe);
+        HashMap<String, List> ingredientRelation = ingredientMatcher.checkIngredientRelation(user,
+            recipe);
         List<Ingredient> have = ingredientRelation.get("have");
         List<Ingredient> need = ingredientRelation.get("need");
 
